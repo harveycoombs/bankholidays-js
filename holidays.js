ae02eb6bc4f81d504ed5a04476549ab957b621d8
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2024-03-23 23:37:28 +0000

    Improvements

--- holidays.js
+++ holidays.js
@@ -1,46 +1,30 @@
 // BankHolidays.JS - Written by Harvey Coombs
-export class BankHoliday {
-    constructor (title, date) {
-        this.title = title;
-        this.date = date;
-    }
-}
-
 export class BankHolidays {
-    static get(filters) {
-        var holidays = [];
-        let request = new XMLHttpRequest();
-
-        request.open("GET", "https://www.gov.uk/bank-holidays.json", false);
-        request.send();
-
-        try {
-            let all = JSON.parse(request.responseText);
-
-            if (all) {
-                let selectedRegion = all[filters.region];
-
-                if (selectedRegion) {
-                    let filtered = selectedRegion.events;
-                    
-                    if (filters.before) {
-                        filtered = filtered.filter(e => (new Date(e.date) < new Date(filters.before)));
-                    }
-
-                    if (filters.after) {
-                        filtered = filtered.filter(e => (new Date(e.date) > new Date(filters.after)));
-                    }
-
-                    for (let holiday of filtered) {
-                        let h = new BankHoliday(holiday.title, holiday.date);
-                        holidays.push(h);
-                    }
-                }
+    static async get(filters) {
+        fetch("https://www.gov.uk/bank-holidays.json").then(response => response.json()).then((all) => {
+            if (!all) {
+                resolve([]);
+                return;        
             }
-        } catch (ex) {
-            throw ex;
-        }
 
-        return holidays;
+            let holidays = [];
+            let selectedRegion = all[filters.region];
+
+            let filtered = selectedRegion?.events;
+
+            if (filters.before) filtered = filtered?.filter(e => (new Date(e.date) < new Date(filters.before))) ?? [];
+            if (filters.after) filtered = filtered?.filter(e => (new Date(e.date) > new Date(filters.after))) ?? [];
+
+            for (let holiday of filtered) {
+                holidays.push({
+                    title: holiday.title,
+                    date: holiday.date
+                });
+            }
+
+            resolve(holidays);
+        }).catch((ex) => {
+            reject(ex);
+        });
     }
 }
